1e0efdfa0e019ea12ec2c8f375b4c8fc6a026e1d
Haseebkhatrikk <73715339+Haseebkhatrikk@users.noreply.github.com>
2020-12-22 01:34:51 +0500

    entityframework

---
+++ UBITIPT/schema.sql
@@ -0,0 +1,90 @@
+﻿USE [IPTServer]
+GO
+/****** Object:  Table [dbo].[Branch]    Script Date: 12/22/2020 1:11:28 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Branch](
+	[Id] [int] IDENTITY(100,100) NOT NULL,
+	[BranchName] [varchar](10) NOT NULL,
+	[BranchManager] [varchar](15) NOT NULL,
+	[BranchAddress] [varchar](20) NOT NULL,
+	[BranchContact] [varchar](15) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Customer]    Script Date: 12/22/2020 1:11:28 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Customer](
+	[Id] [int] IDENTITY(0,1) NOT NULL,
+	[Name] [varchar](10) NOT NULL,
+	[Address] [varchar](20) NOT NULL,
+	[Email] [nvarchar](255) NOT NULL,
+	[PhoneNumber] [varchar](15) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[OrderStatus]    Script Date: 12/22/2020 1:11:28 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[OrderStatus](
+	[Id] [int] IDENTITY(0,1) NOT NULL,
+	[Status] [varchar](15) NOT NULL,
+	[CustomerId] [int] NOT NULL,
+	[ProductId] [int] NOT NULL,
+	[BranchId] [int] NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY],
+ CONSTRAINT [Unique_OrderStatus] UNIQUE NONCLUSTERED 
+(
+	[CustomerId] ASC,
+	[ProductId] ASC,
+	[BranchId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Product]    Script Date: 12/22/2020 1:11:28 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Product](
+	[Id] [int] IDENTITY(1000,5) NOT NULL,
+	[ProductName] [varchar](15) NOT NULL,
+	[ProductWeight] [varchar](10) NOT NULL,
+	[ProductPrice] [varchar](10) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[OrderStatus]  WITH CHECK ADD  CONSTRAINT [FK_OrderStatus_Branch] FOREIGN KEY([BranchId])
+REFERENCES [dbo].[Branch] ([Id])
+GO
+ALTER TABLE [dbo].[OrderStatus] CHECK CONSTRAINT [FK_OrderStatus_Branch]
+GO
+ALTER TABLE [dbo].[OrderStatus]  WITH CHECK ADD  CONSTRAINT [FK_OrderStatus_Customer] FOREIGN KEY([CustomerId])
+REFERENCES [dbo].[Customer] ([Id])
+GO
+ALTER TABLE [dbo].[OrderStatus] CHECK CONSTRAINT [FK_OrderStatus_Customer]
+GO
+ALTER TABLE [dbo].[OrderStatus]  WITH CHECK ADD  CONSTRAINT [FK_OrderStatus_Product] FOREIGN KEY([ProductId])
+REFERENCES [dbo].[Product] ([Id])
+GO
+ALTER TABLE [dbo].[OrderStatus] CHECK CONSTRAINT [FK_OrderStatus_Product]
+GO
